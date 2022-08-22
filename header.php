eab2362fad0133f98b115144b00a20a95426d127
Dillon Ellis <dellis@fcul.com>
2022-08-22 12:56:08 -0400

    updated nav dropdown menus positioning

--- header.php
+++ header.php
@@ -1,171 +1,178 @@
 <!doctype html>
 <html lang="en">
+
 <head>
-<meta charset="utf-8">
+    <meta charset="utf-8">
 
-<link rel="stylesheet" href="https://cuacg.com/remodal/dist/remodal.css">
-<link rel="stylesheet" href="https://cuacg.com/remodal/dist/remodal-default-theme.css">
-<script type="text/javascript">
-piAId = '514811';
-piCId = '169424';
-piHostname = 'pi.pardot.com';
+    <link rel="stylesheet" href="https://cuacg.com/remodal/dist/remodal.css">
+    <link rel="stylesheet" href="https://cuacg.com/remodal/dist/remodal-default-theme.css">
+    <script type="text/javascript">
+        piAId = '514811';
+        piCId = '169424';
+        piHostname = 'pi.pardot.com';
 
-(function() {
-	function async_load(){
-		var s = document.createElement('script'); s.type = 'text/javascript';
-		s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
-		var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
-	}
-	if(window.attachEvent) { window.attachEvent('onload', async_load); }
-	else { window.addEventListener('load', async_load, false); }
-})();
-</script>
+        (function() {
+            function async_load() {
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+                s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+                var c = document.getElementsByTagName('script')[0];
+                c.parentNode.insertBefore(s, c);
+            }
+            if (window.attachEvent) {
+                window.attachEvent('onload', async_load);
+            } else {
+                window.addEventListener('load', async_load, false);
+            }
+        })();
+    </script>
 </head>
-  
+
 <body>
-	
-  
-  <div class="clearfix"></div>
-	<div id="header4">
-	 <div class="container header-new"> 
-		 <div class="row" style="margin-right: 0; margin-left: 0;">
-		  	 <div class="col-xs-4 col-sm-2 col-md-2 col-lg-3">
-				 <a href="index.php" class="toppadd"><img src="https://cuacg.com/images/logo.png" alt="FFCU Logo" style="max-width: 100%; margin-top: 7px;"/></a>
-			 </div>
-			 <div class="col-xs-8 col-sm-7 col-md-7 col-lg-8">
-				 <div class="navbar yamm dark">
 
-          <div class="navbar-header">
-            <button type="button" data-toggle="collapse" data-target="#navbar-collapse-grid" class="navbar-toggle"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-          </div>
 
-          <div id="navbar-collapse-grid" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-            <li class="dropdown"> <a href="https://cuacg.com/index.php" class="dropdown-toggle">Home</a>
-            </li>
-				<li class="dropdown"> <a href="https://cuacg.com/erc/index.php" class="dropdown-toggle">Employee Retention Credit</a>
-            </li>
-             <li class="dropdown yamm-fw"> <a href="audits.php" class="dropdown-toggle">Solutions</a>
+    <div class="clearfix"></div>
+    <div id="header4">
+        <div class="container header-new">
+            <div class="row" style="margin-right: 0; margin-left: 0;">
+                <div class="col-xs-4 col-sm-2 col-md-2 col-lg-3">
+                    <a href="index.php" class="toppadd"><img src="https://cuacg.com/images/logo.png" alt="FFCU Logo" style="max-width: 100%; margin-top: 7px;" /></a>
+                </div>
+                <div class="col-xs-8 col-sm-7 col-md-7 col-lg-8">
+                    <div class="navbar yamm dark">
 
-              <ul class="dropdown-menu">
-				  
-                <li> 
-                  <!-- Content container to add padding -->
-                  <div class="yamm-content">
-                    <div class="row">
-						
-                      <ul class="col-sm-6 col-md-4 list-unstyled ">
-						<h3><a href="https://cuacg.com/audit-solutions.php">AUDIT SOLUTIONS</a></h3>
-						  
-                        <li><a href="https://cuacg.com/supervisory.php">  Supervisory Committee Audit</a></li>
-						  <li><a href="https://cuacg.com/account-verification.php">  Member Account Verification</a></li>
-						  <li><a href="https://cuacg.com/interim.php">  Interim/Internal Audit</a></li>
-						  <li><a href="https://cuacg.com/cash-counts.php">  Cash Counts</a></li>
-                        
-                      </ul>
-						
-                      <ul class="col-sm-6 col-md-4 list-unstyled ">
-						  <h3><a href="https://cuacg.com/compliance-solutions.php">COMPLIANCE SOLUTIONS</a> </h3>
-                        <li><a href="https://cuacg.com/ach.php">  ACH Audit - RDFI & ODFI</a></li>
-						  <li><a href="https://cuacg.com/bsa.php">  Bank Secrecy Act</a></li>
-                         <li><a href="https://cuacg.com/safe.php">  SAFE Act Audit</a></li>
-						  <li><a href="https://cuacg.com/website-review.php">  Website Review</a></li>
-						  <li><a href="https://cuacg.com/ofac.php">  OFAC Assessment</a></li>
-						 
-						  <li><a href="https://cuacg.com/asset-liability.php">  Asset Liability Management Review</a></li>
-						  <li><a href="https://cuacg.com/fair-lending.php">  Fair Lending Review</a></li>
-						   <li><a href="https://cuacg.com/trid.php">  TRID (TILA/RESPA Integrated Disclosures) Review</a></li> 
-                        
-                      </ul>
-                      <ul class="col-sm-6 col-md-4 list-unstyled ">
-						  <h3><a href="https://cuacg.com/management-solutions.php">MANAGEMENT CONSULTING SOLUTIONS</a>
-						  </h3>
-							 <li><a href="https://cuacg.com/fraud.php">  Fraud Audit/Special Investigation </a></li>
-							<li><a href="https://cuacg.com/change-management.php">  Change of Management Review </a></li>
-							<li><a href="https://cuacg.com/supervisory-training.php">  Supervisory Committee Training </a></li>
-							<li><a href="https://cuacg.com/reconciliation.php">  Reconciliation Services </a></li> 
-						  <li><a href="https://cuacg.com/form990.php">  990 Tax Return Preparation </a></li>
-						 	 <li><a href="https://cuacg.com/call-report.php"> 	Call Report Preparation</a></li>
-                        
-                      </ul>
-                      
+                        <div class="navbar-header">
+                            <button type="button" data-toggle="collapse" data-target="#navbar-collapse-grid" class="navbar-toggle"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+                        </div>
+
+                        <div id="navbar-collapse-grid" class="navbar-collapse collapse">
+                            <ul class="nav navbar-nav">
+                                <li class="dropdown"> <a href="https://cuacg.com/index.php" class="dropdown-toggle">Home</a>
+                                </li>
+                                <li class="dropdown"> <a href="https://cuacg.com/erc/index.php" class="dropdown-toggle">Employee Retention Credit</a>
+                                </li>
+                                <li class="dropdown yamm-fw"> <a href="audits.php" class="dropdown-toggle">Solutions</a>
+
+                                    <ul class="dropdown-menu">
+
+                                        <li>
+                                            <!-- Content container to add padding -->
+                                            <div class="yamm-content">
+                                                <div class="row">
+
+                                                    <ul class="col-sm-6 col-md-4 list-unstyled ">
+                                                        <h3><a href="https://cuacg.com/audit-solutions.php">AUDIT SOLUTIONS</a></h3>
+
+                                                        <li><a href="https://cuacg.com/supervisory.php"> Supervisory Committee Audit</a></li>
+                                                        <li><a href="https://cuacg.com/account-verification.php"> Member Account Verification</a></li>
+                                                        <li><a href="https://cuacg.com/interim.php"> Interim/Internal Audit</a></li>
+                                                        <li><a href="https://cuacg.com/cash-counts.php"> Cash Counts</a></li>
+
+                                                    </ul>
+
+                                                    <ul class="col-sm-6 col-md-4 list-unstyled ">
+                                                        <h3><a href="https://cuacg.com/compliance-solutions.php">COMPLIANCE SOLUTIONS</a> </h3>
+                                                        <li><a href="https://cuacg.com/ach.php"> ACH Audit - RDFI & ODFI</a></li>
+                                                        <li><a href="https://cuacg.com/bsa.php"> Bank Secrecy Act</a></li>
+                                                        <li><a href="https://cuacg.com/safe.php"> SAFE Act Audit</a></li>
+                                                        <li><a href="https://cuacg.com/website-review.php"> Website Review</a></li>
+                                                        <li><a href="https://cuacg.com/ofac.php"> OFAC Assessment</a></li>
+
+                                                        <li><a href="https://cuacg.com/asset-liability.php"> Asset Liability Management Review</a></li>
+                                                        <li><a href="https://cuacg.com/fair-lending.php"> Fair Lending Review</a></li>
+                                                        <li><a href="https://cuacg.com/trid.php"> TRID (TILA/RESPA Integrated Disclosures) Review</a></li>
+
+                                                    </ul>
+                                                    <ul class="col-sm-6 col-md-4 list-unstyled ">
+                                                        <h3><a href="https://cuacg.com/management-solutions.php">MANAGEMENT CONSULTING SOLUTIONS</a>
+                                                        </h3>
+                                                        <li><a href="https://cuacg.com/fraud.php"> Fraud Audit/Special Investigation </a></li>
+                                                        <li><a href="https://cuacg.com/change-management.php"> Change of Management Review </a></li>
+                                                        <li><a href="https://cuacg.com/supervisory-training.php"> Supervisory Committee Training </a></li>
+                                                        <li><a href="https://cuacg.com/reconciliation.php"> Reconciliation Services </a></li>
+                                                        <li><a href="https://cuacg.com/form990.php"> 990 Tax Return Preparation </a></li>
+                                                        <li><a href="https://cuacg.com/call-report.php"> Call Report Preparation</a></li>
+
+                                                    </ul>
+
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </li>
+
+                                <li class="dropdown yamm-fw"> <a href="https://cuacg.com/about-us.php" class="dropdown-toggle">About Us</a>
+
+                                    <ul class="dropdown-menu" style="left: 361px;">
+
+                                        <li>
+                                            <!-- Content container to add padding -->
+                                            <div class="yamm-content">
+                                                <div class="row">
+                                                    <ul class="col-sm-6 col-md-3 list-unstyled ">
+
+                                                        <li><a href="https://cuacg.com/audit-team.php"> Audit Team</a></li>
+                                                        <li><a href="https://cuacg.com/dashboard.php"> Dashboard</a></li>
+                                                        <!--<li><a href="verified-visa.php">  Verified by Visa</a></li>-->
+
+                                                    </ul>
+
+                                                </div>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class=""> <a href="https://cuacg.com/connect.php" class="dropdown-toggle">Connect</a>
+
+                                </li>
+                                <li class=""> <a href="https://cuacg.com/resources.php" class="dropdown-toggle">Resources</a>
+
+                                </li>
+
+                                <li class="dropdown yamm-fw"> <a href="#" class="dropdown-toggle">Questionnaires</a>
+
+                                    <ul class="dropdown-menu" style="left: 595px;">
+
+                                        <li>
+                                            <!-- Content container to add padding -->
+                                            <div class="yamm-content">
+                                                <div class="row">
+                                                    <ul class="col-sm-6 col-md-12 list-unstyled ">
+                                                        <li><a href="https://cuacg.com/ach-questionnaire.php"> ACH Questionnaire</a></li>
+                                                        <li><a href="https://cuacg.com/bsa-questionnaire.php"> BSA Questionnaire</a></li>
+                                                        <li><a href="https://cuacg.com/erc-questionnaire.php"> ERC Questionnaire</a></li>
+                                                        <li><a href="https://cuacg.com/fairlending-questionnaire.php"> Fair Lending Questionnaire</a></li>
+                                                        <li><a href="https://cuacg.com/member-questionnaire.php"> Member Account Verification Questionnaire</a></li>
+                                                        <li><a href="https://cuacg.com/supervisory-questionnaire.php"> Supervisory Committee Audit Questionnaire</a></li>
+                                                        <li><a href="https://cuacg.com/fairlending-loan-officer-questionnaire.php"> Fairlending Loan Officer Questionnaire</a></li>
+                                                        <li><a href="https://cuacg.com/fairlending-marketing-questionnaire.php"> Fairlending Marketing Questionnaire</a></li>
+                                                        <li><a href="https://cuacg.com/safe-act-questionnaire.php"> Safe Act Questionnaire</a></li>
+                                                        <li><a href="https://cuacg.com/website-review-questionnaire.php"> Website Review Questionnaire</a></li>
+
+                                                    </ul>
+
+
+
+                                                </div>
+                                        </li>
+                                    </ul>
+                                </li>
+
+                            </ul>
+
+                        </div>
                     </div>
-                  </div>
-                </li>
-              </ul>
-            </li>
-            
-            <li class="dropdown yamm-fw"> <a href="https://cuacg.com/about-us.php" class="dropdown-toggle">About Us</a>
+                </div>
+                <div class="col-xs-1 col-sm-3 col-md-3 col-lg-2">
+                    <!--<div class="search"><input class="search-box" name="q" title="search-box" type="text" value="search"  onclick="this.value='';" onfocus="this.select()" onblur="this.value=!this.value?'search...':this.value;"><input type="image" src="images/search.png" alt="Submit" class="search-btn"></div>-->
+                </div>
+                <!--<button type="button" data-toggle="collapse" data-target="#online-collapse-grid" class="navbar-toggle banking-toggle"><span class="online-btn">Online Banking</span></button>-->
+                <div class="col-xs-7 col-sm-6 col-md-4 col-lg-4">
 
-              <ul class="dropdown-menu" style="left: 170px;">
-				  
-                <li> 
-                  <!-- Content container to add padding -->
-                  <div class="yamm-content">
-                    <div class="row">
-                      <ul class="col-sm-6 col-md-3 list-unstyled ">
 
-                        <li><a href="https://cuacg.com/audit-team.php">  Audit Team</a></li>
-                        <li><a href="https://cuacg.com/dashboard.php">  Dashboard</a></li>
-						  <!--<li><a href="verified-visa.php">  Verified by Visa</a></li>-->
-                        
-                      </ul>
-                     
-                  </div>
-                </li>
-              </ul>
-            </li>
-            <li class=""> <a href="https://cuacg.com/connect.php" class="dropdown-toggle">Connect</a>
-           
-            </li>
-            <li class=""> <a href="https://cuacg.com/resources.php" class="dropdown-toggle">Resources</a>
-             
-            </li>
-            
-			<li class="dropdown yamm-fw"> <a href="#" class="dropdown-toggle">Questionnaires</a>
-
-              <ul class="dropdown-menu" style="left: 370px;">
-				  
-                <li> 
-                  <!-- Content container to add padding -->
-                  <div class="yamm-content">
-                    <div class="row">
-                      <ul class="col-sm-6 col-md-12 list-unstyled ">
-						<li><a href="https://cuacg.com/ach-questionnaire.php">  ACH Questionnaire</a></li>
-						  <li><a href="https://cuacg.com/bsa-questionnaire.php">  BSA Questionnaire</a></li>
-						  <li><a href="https://cuacg.com/erc-questionnaire.php">  ERC Questionnaire</a></li>
-                        <li><a href="https://cuacg.com/fairlending-questionnaire.php">  Fair Lending Questionnaire</a></li>
-						  <li><a href="https://cuacg.com/member-questionnaire.php">  Member Account Verification Questionnaire</a></li>
-                        <li><a href="https://cuacg.com/supervisory-questionnaire.php">  Supervisory Committee Audit Questionnaire</a></li>
-						  <li><a href="https://cuacg.com/fairlending-loan-officer-questionnaire.php">  Fairlending Loan Officer Questionnaire</a></li>
-						  <li><a href="https://cuacg.com/fairlending-marketing-questionnaire.php">  Fairlending Marketing Questionnaire</a></li>
-						  <li><a href="https://cuacg.com/safe-act-questionnaire.php">  Safe Act Questionnaire</a></li>
-						  <li><a href="https://cuacg.com/website-review-questionnaire.php">  Website Review Questionnaire</a></li>
-
-                      </ul>
-						
-						
-                     
-                  </div>
-                </li>
-              </ul>
-            </li>	
-
-          </ul>
-           
-          </div>
+                </div>
+            </div>
         </div>
-			 </div>
-			 <div class="col-xs-1 col-sm-3 col-md-3 col-lg-2">
-				 <!--<div class="search"><input class="search-box" name="q" title="search-box" type="text" value="search"  onclick="this.value='';" onfocus="this.select()" onblur="this.value=!this.value?'search...':this.value;"><input type="image" src="images/search.png" alt="Submit" class="search-btn"></div>-->
-			 </div>
-			 <!--<button type="button" data-toggle="collapse" data-target="#online-collapse-grid" class="navbar-toggle banking-toggle"><span class="online-btn">Online Banking</span></button>-->
-			 <div class="col-xs-7 col-sm-6 col-md-4 col-lg-4">
-				 
-				 
-		 </div>
     </div>
-		 </div>
-</div>
 </body>
+
 </html>
